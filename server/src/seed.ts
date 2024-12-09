dc1bacf703d201608a2f02ccdaf253af95bcd7ab
sahir <sahir@pop-os.localdomain>
2024-12-09 11:45:05 +0530

    fix: seed file

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -1,12 +1,24 @@
+import mongoose from "mongoose";
 import { SuportedLanguagesModel } from "./Models/SuportedLanguages";
+import { config } from "dotenv";
+config()
 
-SuportedLanguagesModel.insertMany([
-    {
-        language:"javascript",
-        image:"code-sandbox-js"
-    },
-    {
-        language:"python",
-        image:"code-sandbox-py"
-    }
-])
+console.log(process.env.MONGODB_URI || "")
+mongoose.connect(process.env.MONGODB_URI || "")
+.then(async()=>{
+    await SuportedLanguagesModel.insertMany([
+        {
+            language:"javascript",
+            image:"code-sandbox-js"
+        },
+        {
+            language:"python",
+            image:"code-sandbox-py"
+        }
+    ])
+    mongoose.connection.close()
+})
+.catch(e=>{
+    console.log("Error connecting to mongodb")
+    console.log(e)
+})
